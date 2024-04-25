9a8162a76952fc598ad49d4e745f5d828e758794
Kim Nymark <kim.nymark@edu.novia.fi>
2024-04-25 16:00:37 +0300

    Wörking version, with splashscreen, icons and gifs

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+import logoImage from '../assets/sad-cat-cat-in-front-of-sea.gif';
 
 const ErrorPage = () => (
   <div className="error-page text-center">
     <h1>Oops! Page Not Found</h1>
     <p>The page you are looking for might have been removed or is temporarily unavailable.</p>
     <p>Head back to <Link to="/">Home</Link>.</p>
-    <img src="public/sad-cat-cat-in-front-of-sea.gif" alt="Error 404" className="img-fluid" />
+    <img src={logoImage} alt="Error 404" className="img-fluid" />
   </div>
 );
 
